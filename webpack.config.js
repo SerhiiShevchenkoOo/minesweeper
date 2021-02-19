40640cead3e9e93cb5bf2467799d5d6dbcfc5baa
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-02-19 18:55:49 +0200

    feat: update webpack4>webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -9,51 +9,52 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
-(autoprefixer = require('autoprefixer')),
-	(plugin = autoprefixer({ grid: true })),
-	(optimization = () => {
-		const config = {
-			splitChunks: {
-				chunks: 'all',
-			},
-		};
-		if (isProd) {
-			config.minimizer = [
-				new OptimizeCssAssetPlugin(),
-				new TerserWebpackPlugin(),
-			];
-		}
-		return config;
-	}),
-	(cssLoader = extra => {
-		const loaders = [
-			{
-				loader: MiniCssExtractPlugin.loader,
-				options: {
-					hmr: isDev,
-					reloadAll: true,
-				},
-			},
-			'css-loader',
-			{
-				loader: 'postcss-loader',
-			},
+
+const optimization = () => {
+	const config = {
+		splitChunks: {
+			chunks: 'all',
+		},
+	};
+	if (isProd) {
+		config.minimizer = [
+			new OptimizeCssAssetPlugin(),
+			new TerserWebpackPlugin(),
 		];
-		if (extra) {
-			loaders.push(extra);
-		}
-		return loaders;
-	}),
-	(babelOptions = preset => {
-		opts = {
-			presets: ['@babel/preset-env'],
-			plugins: ['@babel/plugin-proposal-class-properties'],
-		};
-		if (preset) {
-			opts.presets.push(preset);
-		}
-		return opts;
-	});
+	}
+	return config;
+};
+
+const cssLoader = extra => {
+	const loaders = [
+		{
+			loader: MiniCssExtractPlugin.loader,
+			options: {
+				hmr: isDev,
+				reloadAll: true,
+			},
+		},
+		'css-loader',
+		{
+			loader: 'postcss-loader',
+		},
+	];
+	if (extra) {
+		loaders.push(extra);
+	}
+	return loaders;
+};
+
+const babelOptions = preset => {
+	opts = {
+		presets: ['@babel/preset-env'],
+		plugins: ['@babel/plugin-proposal-class-properties'],
+	};
+	if (preset) {
+		opts.presets.push(preset);
+	}
+	return opts;
+};
 
 module.exports = {
 	context: path.resolve(__dirname, '#src'),
@@ -86,21 +87,9 @@ module.exports = {
 
 	plugins: [
 		new ESLintPlugin(),
-
-		// new CopyWebpackPlugin({
-		// 	patterns: [
-		// 		{
-		// 			from: './assets',
-		// 			to: './assets',
-		// 		},
-		// 	],
-		// }),
 		new HTMLWebpackPlugin({
-			filename: './index.html',
+			filename: 'index.html',
 			template: './index.html',
-			minify: {
-				collapseWhitespace: isProd,
-			},
 		}),
 
 		new MiniCssExtractPlugin({
@@ -113,21 +102,15 @@ module.exports = {
 		rules: [
 			{
 				test: /\.html$/,
-				include: path.resolve(__dirname, '#src/includes'),
 				use: ['raw-loader'],
 			},
-
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: {
+				use: {
 					loader: 'babel-loader',
 					options: babelOptions(),
 				},
-			},
-			{
-				test: /\.less$/,
-				use: cssLoader('less-loader'),
 			},
 			{
 				test: /\.css$/,
@@ -146,50 +129,36 @@ module.exports = {
 					},
 				],
 			},
+
 			{
 				test: /\.s[ac]ss$/,
 				use: cssLoader('sass-loader'),
 			},
+
 			{
 				test: /\.(png|jpg|svg|gif|webp)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: '[path][name].[ext]',
-						outputPath: './',
-					},
-				},
+				type: 'asset/resource',
 			},
+
 			{
 				test: /\.(ttf|woff|woff2|eot)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: '[path][name].[ext]',
-						outputPath: './',
-					},
-				},
+				type: 'asset/resource',
 			},
+
 			{
 				test: /\.xml$/,
 				use: ['xml-loader'],
 			},
+
 			{
 				test: /\.csv$/,
 				use: ['cvs-loader'],
 			},
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				loader: {
-					loader: 'babel-loader',
-					options: babelOptions('@babel/preset-typescript'),
-				},
-			},
+
 			{
 				test: /\.jsx$/,
 				exclude: /node_modules/,
-				loader: {
+				use: {
 					loader: 'babel-loader',
 					options: babelOptions('@babel/preset-react'),
 				},
