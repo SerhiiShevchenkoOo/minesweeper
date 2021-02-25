089ae872d4e3b70fcec778385ac55ee1b87e3da6
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-02-25 15:17:06 +0200

    add: audio files

---
+++ #src/assets/songs/song.js
@@ -0,0 +1,14 @@
+export let songs = [
+	new Audio('./assets/songs/Sound_19240.mp3'),
+	new Audio('./assets/songs/Sound_19241.mp3'),
+	new Audio('./assets/songs/Sound_19255.mp3'),
+	new Audio('./assets/songs/Sound_19263.mp3'),
+	new Audio('./assets/songs/Sound_19321.mp3'),
+	new Audio('./assets/songs/start.wav'),
+	new Audio('./assets/songs/Sound_06769.mp3'),
+];
+// click
+export let activeSong = {
+	openMo: new Audio('./assets/songs/focus.mp3'),
+	click: new Audio('./assets/songs/onclick.mp3'),
+};
