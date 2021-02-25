3b7dd3a5e34cc9df1bbd523b23d2a72188c3062e
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-02-25 15:21:48 +0200

    feat: add style for timer

--- #src/script/component/Timer.jsx
+++ #src/script/component/Timer.jsx
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react';
-
-export default ({ isActive, time }) => {
-	const [seconds, setSeconds] = useState(time);
-
+const colors = [
+	'bg-purple-50',
+	'bg-purple-100',
+	'bg-purple-200',
+	'bg-purple-300',
+	'bg-purple-400',
+	'bg-purple-500',
+	'bg-purple-600',
+	'bg-purple-700',
+	'bg-purple-800',
+	'bg-purple-900',
+];
+export default ({ isActive, time, setTime }) => {
 	useEffect(() => {
-		setSeconds(time);
 		let interval = null;
-
 		interval = setInterval(() => {
-			setSeconds(seconds => seconds + 1);
+			setTime(time => time + 1);
 		}, 1000);
 
 		return () => clearInterval(interval);
 	}, [time, isActive]);
 
-	return <div className='w-10 h-10 bg-red-700 justify-center '>{seconds}</div>;
+	return (
+		<div
+			className={` bg-black px-2 py-1 rounded-xl font-medium text-xl  text-yellow-500 transition duration-500 justify-center flex items-center`}>
+			<p>Time: {time}s</p>
+		</div>
+	);
 };
