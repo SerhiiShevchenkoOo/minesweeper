9dfd8c1264eca05a3b3bb77a62c01036c398ab4b
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-02-25 15:19:52 +0200

    feat: add stules for game field

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,21 @@ module.exports = {
 	theme: {
 		extend: {
 			gridTemplateColumns: {
-				16: 'repeat(16, minmax(0, 1fr))',
-				30: 'repeat(30, minmax(0, 1fr))',
+				16: 'repeat(16, minmax(30px, 30px))',
+				30: 'repeat(30, minmax(25px, 25px))',
+				9: 'repeat(9, minmax(30px, 30px))',
+				'16t': 'repeat(16, minmax(45px, 45px))',
+				'30t': 'repeat(30, minmax(45px, 45px))',
+				'9t': 'repeat(9, minmax(45px, 45px))',
 			},
+			gridAutoRows: {
+				1: '30px',
+				2: '25px',
+				t: '45px',
+			},
+		},
+		maxHeight: {
+			'3/4': '75%',
 		},
 	},
 	variants: {
