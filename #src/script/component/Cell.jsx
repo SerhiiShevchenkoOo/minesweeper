80911efc7db4b4bc33fb34d5c0034df87a07d01e
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-02-27 16:53:26 +0200

    feat: add speaker functions for cell components

--- #src/script/component/Cell.jsx
+++ #src/script/component/Cell.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect, useContext } from 'react';
 import bomb from '@assets/images/bomb.svg';
 import checkImage from '@assets/images/error.svg';
 import storage from '@utils/storage.js';
-import next from '@utils/game-open-logic.js';
+import checkNextCell from '@utils/game-open-logic.js';
 import PropTypes from 'prop-types';
 import pause from '@utils/pause.js';
 import { Context } from '../utils/Context';
-import { activeSong } from '@assets/songs/song.js';
-
+import song from '@assets/songs/song.js';
+import useSound from 'use-sound';
 const Cell = ({
 	cell,
 	index,
@@ -20,25 +20,38 @@ const Cell = ({
 	updatePoint,
 	reset,
 }) => {
-	const { contextValue, ContextActive } = useContext(Context);
+	const { contextValue, ContextActive, volume } = useContext(Context);
+	const [playbackRate, setPlaybackRate] = volume;
 	const [speakOn, setSpeakOn] = ContextActive;
 	const [speakValue, setSpeakValue] = contextValue;
 	const [cleanup, setCleanup] = useState(false);
 	const [open, setOpen] = useState(cell.open); // open cell  true/false
 	const [check, setCheck] = useState(cell.check); // chack cell true/false
 	const [scale, setScale] = useState('');
+
+	const [playCheck] = useSound(song.activeSong[0], {
+		volume: playbackRate,
+	});
+	const [playClick] = useSound(song.activeSong[1], {
+		volume: playbackRate,
+	});
+
 	const openCell = () => {
 		cell.check && setMineSensor(false, arr); // if cell.checked> mineSensor++
-		next(arr, index, cell, updatePoint, setGameOver, update, cut); //
+		!cell.open && !cell.mine && playClick();
+		checkNextCell(arr, index, cell, updatePoint, setGameOver, update, cut); //
 	}; //Click
+
 	// set check
 	const checkedCell = e => {
 		e && e.preventDefault();
 		if (cell.open) return;
 		setCheck(!check);
+		playCheck();
 		!cell.check ? setMineSensor(true, arr) : setMineSensor(false, arr);
 		cell.check = check;
 	}; // contextMenu event
+
 	// set bacground
 	const setBg = () =>
 		open
@@ -50,10 +63,12 @@ const Cell = ({
 			: 'bg-gray-400';
 
 	cell.check = check;
+
 	useEffect(() => {
 		setOpen(cell.open);
 		setCheck(cell.check);
 	}, [update, checkedCell]); // open cell if next cell open
+
 	useEffect(() => {
 		reset && setCheck(false);
 		reset && setOpen(false);
@@ -61,18 +76,20 @@ const Cell = ({
 		setCheck(false);
 		setOpen(false);
 	}, [newGame, cut]); // if start new game/ change field size
+
 	useEffect(() => {
 		return () => setCleanup(true);
 	}, []); // reset scale
+
 	useEffect(() => {
 		const numberCell = speakValue.split(' ')[0];
 		const move = speakValue.split(' ')[1];
 		if (+numberCell === index) {
-			console.log(move);
-			move === 'открыть' && openCell();
-			move == 'закрыть' && checkedCell();
+			move == '+' && openCell();
+			move == '-' && checkedCell();
 		}
 	}, [speakValue]);
+
 	return (
 		<button
 			onMouseOver={() => {
