c2898983fbe296e045a2e41cb25c6a7745250138
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-02-27 16:53:56 +0200

    fix: fix song.js

--- #src/assets/songs/song.js
+++ #src/assets/songs/song.js
@@ -1,14 +1,14 @@
-export let songs = [
-	new Audio('./assets/songs/Sound_19240.mp3'),
-	new Audio('./assets/songs/Sound_19241.mp3'),
-	new Audio('./assets/songs/Sound_19255.mp3'),
-	new Audio('./assets/songs/Sound_19263.mp3'),
-	new Audio('./assets/songs/Sound_19321.mp3'),
-	new Audio('./assets/songs/start.wav'),
-	new Audio('./assets/songs/Sound_06769.mp3'),
-];
+import song1 from './Sound_19240.mp3';
+import song2 from './Sound_19241.mp3';
+import song3 from './Sound_19255.mp3';
+import song4 from './Sound_19263.mp3';
+import song5 from './start.wav';
+import song6 from './Sound_06769.mp3';
+import openNextCellAudio from './focus.mp3';
+import clickCellAudio from './onclick.mp3';
+
+const songs = [song6, song5, song4, song3, song2, song1];
 // click
-export let activeSong = {
-	openMo: new Audio('./assets/songs/focus.mp3'),
-	click: new Audio('./assets/songs/onclick.mp3'),
-};
+const activeSong = [clickCellAudio, openNextCellAudio];
+
+export default { songs, activeSong };
